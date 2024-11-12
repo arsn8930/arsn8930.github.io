8522e26821faf261adac1142c97276cd8aa0ee62
arsn8930 <arsn8930@colorado.edu>
2024-11-12 12:35:39 -0700

    ica12

---
+++ js/ica12.js
@@ -0,0 +1,46 @@
+// This is my API
+const jokeApiEndpoint = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=singleLinks"; 
+
+// defning button
+const button = document.getElementById('fetch_stuff_btn')
+// defining endpoint
+let apiEndpoint = jokeApiEndpoint; 
+
+// event listener
+document.getElementById('fetch_stuff_btn').addEventListener('click', getQuote);
+
+// function to get the quote and check the log
+function getQuote(){
+    console.log("Button was clicked, fecthing the stuff...")
+
+    // fetching
+    fetch(apiEndpoint)
+        .then(response =>{
+            if(!response.ok) {
+                // errors
+                throw new Error("I have failed!!!! Data not fetched!!!") 
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (apiEndpoint === jokeApiEndpoint){
+                if (data.type === 'single'){
+                    displayRes(data.joke);
+                }
+                else {
+                    displayRes(`${data.setup} - ${data.delivery}`);
+                }
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error)
+            alert("Ooops! you fucked up. Try again...or don't")
+        })
+}
+
+function displayRes(quote) {
+    const quoteDisplay = document.getElementById('quote_display');
+    quoteDisplay.textContent = quote;
+}
+
+button.addEventListener('click', getQuote);
